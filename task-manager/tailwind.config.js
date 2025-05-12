d8dfaebcc7629a243127ab5d3292a24129e65fbd
ahasn-9 <mughalahsan718@gmail.com>
2025-05-12 11:54:34 -0700

    set react + tailwind envirnment

---
+++ task-manager/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Adjust the path based on your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
